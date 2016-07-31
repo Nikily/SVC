507ceb5f95aa2d7458d147b816ee8e2bc9197442
Nikily <nikolailich@gmail.com>
2016-08-01 01:46:32 +0200

    Add files via upload

---
+++ Console Saving manager/Console Saving manager/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Saving_manager
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.Write(" ******* Welcome to your saving account  ******* \n\nPlease enter amount you would like to add (quit by typing 'quit'): ");
+            savingsAccount();
+        }
+        private static void savingsAccount()
+        {
+
+            string exit = "quit";
+            string input;
+            float totalAmount = 0, amountValue = 0;
+            do
+            {
+                input = Console.ReadLine();
+                bool inputBool = float.TryParse(input, out amountValue);
+
+                if (inputBool == true) 
+                {
+
+                    float[] history; // = new float[] 
+            
+                        }
+
+                    if ((totalAmount + amountValue) < 1000)
+                    {
+                        totalAmount += amountValue;
+                        if (totalAmount < 100) { Console.WriteLine("Great!! Add more value, your current balance total is only: {0:C}", totalAmount); }
+                        else { Console.WriteLine("Your current balance total is: {0:C}, add more if you like please", totalAmount); }
+                    }
+
+                    else { Console.WriteLine("Your total would be over $ 1000 ({0:C}), don't you think that would be too much? Add less, or quit by typing 'quit'", totalAmount + amountValue); }
+                }
+                else { Console.WriteLine("Please enter a numerical value"); }
+                    
+ 
+            } while (input.ToLower() != exit);
+        }
+       }
+}
---
+++ SimpleCalculator/CreateArrayForCalc/CreateArrayForCalc/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CreateArrayForCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to a really cool calculator *****");
+
+            string exit = "quit";
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+                input = input.Replace(" ", "");
+
+                if (input.ToLower() == exit)
+                { Environment.Exit(0); }
+
+                char[] operators = new char[] { '+', '-', '*', '/' };
+                string[] strnumbers = input.Split(operators);
+                string[] strOperators = Regex.Split(input, @"\d+");
+
+
+                string FinOpString = "";
+
+                for (int i = 0; i < strOperators.Length; i++)
+                {
+
+                    if (strOperators[i] == "+" || strOperators[i] == "-" || strOperators[i] == "*" || strOperators[i] == "/")
+                    {
+                        FinOpString += strOperators[i];
+                    }
+                }
+
+                char[] operatorsArray = FinOpString.ToCharArray();
+
+
+                try
+                {
+
+                    float[] floatNumbers = new float[strnumbers.Length];
+                    for (int i = 0; i < strnumbers.Length; i++)
+                    {
+                        floatNumbers[i] = float.Parse(strnumbers[i]);
+                    }
+
+
+
+                    float result = float.Parse(strnumbers[0]);
+                    int n = 0;
+                    if (strnumbers.Length == 1)
+                    {
+                        Console.WriteLine("Please enter more than one value!");
+                    }
+                    else
+                    {
+                        while (n < 1)
+
+                        {
+                            for (int i = 0; i < operatorsArray.Length; i++)
+                            {
+
+                                if (operatorsArray[i] == '+')
+                                { result += floatNumbers[i + 1]; }
+                                if (operatorsArray[i] == '-')
+                                { result -= floatNumbers[i + 1]; }
+                                if (operatorsArray[i] == '*')
+                                { result *= floatNumbers[i + 1]; }
+                                if (operatorsArray[i] == '/')
+                                { result /= floatNumbers[i + 1]; }
+                                n++;
+                                Console.WriteLine("Result: {0}\nEnter new values to calculate or quit by typing 'quit'", result);
+                            }
+
+                        }
+                    }
+                }
+
+                catch (System.OverflowException)
+                {
+                    Console.WriteLine("One or more values is too large for calculations,enter value in the range " +
+                     "of 3.4E +/- 38 and try again!");
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Please enter numerical values :) !");
+                }
+               
+            
+         }while (input.ToLower() != exit);
+         Console.ReadLine();
+        }
+    }
+}
+   
+
