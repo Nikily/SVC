15844fb5c1fa5f023b3162271635018a6390bb04
Nikili <Nikili@BEL-NIKILI-LT02>
2016-08-01 00:36:01 +0200

    Exceptions handled, verification for numerical values, and also verification on the number of values.

--- CreateArrayForCalc/CreateArrayForCalc/Program.cs
+++ CreateArrayForCalc/CreateArrayForCalc/Program.cs
@@ -11,59 +11,94 @@ namespace CreateArrayForCalc
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            input = input.Replace(" ", "");
+            Console.WriteLine("***** Welcome to a really cool calculator *****");
 
-            Console.WriteLine("This is the string for the array: {0}", input);
+            string exit = "quit";
+            string input;
 
-            char[] operators = new char[] { '+', '-', '*', '/' };
-            string[] strnumbers = input.Split(operators);
-            string[] strOperators = Regex.Split(input,@"\d+");
-            //strOperators = Regex.Split
-
-            
-            foreach (string number in strnumbers)
+            do
             {
-                Console.Write("{0}" + " \n", number);
-            }
+                input = Console.ReadLine();
+                input = input.Replace(" ", "");
 
-            string FinOpString = "";
+                if (input.ToLower() == exit)
+                { Environment.Exit(0); }
 
-            for (int i = 0; i < strOperators.Length; i++)
-            {
-               
-                if (strOperators[i] == "+" || strOperators[i] == "-" || strOperators[i] == "*" || strOperators[i] == "/")
+                char[] operators = new char[] { '+', '-', '*', '/' };
+                string[] strnumbers = input.Split(operators);
+                string[] strOperators = Regex.Split(input, @"\d+");
+
+
+                string FinOpString = "";
+
+                for (int i = 0; i < strOperators.Length; i++)
                 {
-                    FinOpString += strOperators[i];
+
+                    if (strOperators[i] == "+" || strOperators[i] == "-" || strOperators[i] == "*" || strOperators[i] == "/")
+                    {
+                        FinOpString += strOperators[i];
+                    }
                 }
-            }
 
-            char[] operatorsArray = FinOpString.ToCharArray();
+                char[] operatorsArray = FinOpString.ToCharArray();
 
+
+                try
+                {
+
+                    float[] floatNumbers = new float[strnumbers.Length];
+                    for (int i = 0; i < strnumbers.Length; i++)
+                    {
+                        floatNumbers[i] = float.Parse(strnumbers[i]);
+                    }
+
+
+
+                    float result = float.Parse(strnumbers[0]);
+                    int n = 0;
+                    if (strnumbers.Length == 1)
+                    {
+                        Console.WriteLine("Please enter more than one value!");
+                    }
+                    else
+                    {
+                        while (n < 1)
+
+                        {
+                            for (int i = 0; i < operatorsArray.Length; i++)
+                            {
+
+                                if (operatorsArray[i] == '+')
+                                { result += floatNumbers[i + 1]; }
+                                if (operatorsArray[i] == '-')
+                                { result -= floatNumbers[i + 1]; }
+                                if (operatorsArray[i] == '*')
+                                { result *= floatNumbers[i + 1]; }
+                                if (operatorsArray[i] == '/')
+                                { result /= floatNumbers[i + 1]; }
+                                n++;
+                                Console.WriteLine("Result: {0}\nEnter new values to calculate or quit by typing 'quit'", result);
+                            }
+
+                        }
+                    }
+                }
+
+                catch (System.OverflowException)
+                {
+                    Console.WriteLine("One or more values is too large for calculations,enter value in the range " +
+                     "of 3.4E +/- 38 and try again!");
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Please enter numerical values :) !");
+                }
+               
             
-            foreach (char oprt in operatorsArray)
-            {
-                Console.WriteLine("This are the operators: {0} ", oprt);
-            }
-
-            int result = Int32.Parse(strnumbers[0]);
-
-            for (int i = 0; i < operatorsArray.Length; i++)
-            {
-                
-                if(operatorsArray[i] == '+')
-                { result += Int32.Parse(strnumbers[i+1]); }
-                if(operatorsArray[i] == '-')
-                { result -= Int32.Parse(strnumbers[i + 1]); }
-                if (operatorsArray[i] == '*')
-                { result *= Int32.Parse(strnumbers[i + 1]); }
-                if (operatorsArray[i] == '/')
-                { result /= Int32.Parse(strnumbers[i + 1]); }
-            }
-
-            Console.WriteLine("This is the results: {0}",result );
-
-            Console.ReadLine();
+         }while (input.ToLower() != exit);
+         Console.ReadLine();
         }
     }
 }
+   
+
