644604eb53eb7ce63dc3ae61ce275230b80140dd
Nikili <Nikili@BEL-NIKILI-LT02>
2016-08-03 01:50:25 +0200

    Created a new class Calculate which contains the logic of calculation

--- CreateArrayForCalc/CreateArrayForCalc/Calculate.cs
+++ CreateArrayForCalc/CreateArrayForCalc/Calculate.cs
@@ -7,67 +7,63 @@ using System.Text.RegularExpressions;
 
 namespace CreateArrayForCalc
 {
-    class Program
+    public class Calculate
     {
-        static void Main(string[] args)
+        public static string exit = "quit";
+        public static string input;
+        public static float result;
+        public static string FinOpString;
+        public static char[] operators;
+        public static string[] strnumbers;
+        public static string[] strOperators;
+        public static char[] operatorsArray;
+        public void Result()
         {
-            Console.WriteLine("***** Welcome to a really cool calculator *****");
-
-            string exit = "quit";
-            string input;
 
             do
             {
-                input = Console.ReadLine();
-                input = input.Replace(" ", "");
+              input = Console.ReadLine();
+              input = input.Replace(" ", "");
+              FinOpString = "";
 
-                if (input.ToLower() == exit)
+              if (input.ToLower() == exit)
                 { Environment.Exit(0); }
 
-                char[] operators = new char[] { '+', '-', '*', '/' };
-                string[] strnumbers = input.Split(operators);
-                string[] strOperators = Regex.Split(input, @"\d+");
-
-
-                string FinOpString = "";
+                operators = new char[] { '+', '-', '*', '/' };
+                strnumbers = input.Split(operators);
+                strOperators = Regex.Split(input, @"\d+");
 
                 for (int i = 0; i < strOperators.Length; i++)
                 {
-
                     if (strOperators[i] == "+" || strOperators[i] == "-" || strOperators[i] == "*" || strOperators[i] == "/")
                     {
                         FinOpString += strOperators[i];
                     }
                 }
 
-                char[] operatorsArray = FinOpString.ToCharArray();
-
+                operatorsArray = FinOpString.ToCharArray();
 
                 try
                 {
-
                     float[] floatNumbers = new float[strnumbers.Length];
-                    for (int i = 0; i < strnumbers.Length; i++)
+                    for ( int i = 0; i<strnumbers.Length; i++)
                     {
                         floatNumbers[i] = float.Parse(strnumbers[i]);
                     }
 
-
-
-                    float result = float.Parse(strnumbers[0]);
+                    result = float.Parse(strnumbers[0]);
                     int n = 0;
+
                     if (strnumbers.Length == 1)
                     {
                         Console.WriteLine("Please enter more than one value!");
                     }
                     else
                     {
-                        while (n < 1)
-
+                        while(n < 1)
                         {
                             for (int i = 0; i < operatorsArray.Length; i++)
                             {
-
                                 if (operatorsArray[i] == '+')
                                 { result += floatNumbers[i + 1]; }
                                 if (operatorsArray[i] == '-')
@@ -79,26 +75,22 @@ namespace CreateArrayForCalc
                                 n++;
                                 Console.WriteLine("Result: {0}\nEnter new values to calculate or quit by typing 'quit'", result);
                             }
-
                         }
                     }
                 }
-
                 catch (System.OverflowException)
                 {
                     Console.WriteLine("One or more values is too large for calculations,enter value in the range " +
                      "of 3.4E +/- 38 and try again!");
                 }
-                catch (System.FormatException)
+                catch(System.FormatException)
                 {
-                    Console.WriteLine("Please enter numerical values :) !");
+                    Console.WriteLine("Please enter numerical values !");
                 }
-               
-            
-         }while (input.ToLower() != exit);
-         Console.ReadLine();
-        }
-    }
+
+            } while (input.ToLower() != exit);
+         }
+     }
 }
-   
+    
 
--- CreateArrayForCalc/CreateArrayForCalc/Program.cs
+++ CreateArrayForCalc/CreateArrayForCalc/Program.cs
@@ -12,106 +12,13 @@ namespace CreateArrayForCalc
         static void Main(string[] args)
         {
             Console.WriteLine("***** Welcome to a really cool calculator *****");
+            Calculate NewCalculation = new Calculate();
 
-            Calculate newCalculation = new Calculate();
-            newCalculation.Result();
-
+            NewCalculation.Result();
             Console.ReadLine();
-
         }
-    }
+    }  
+ }
 
-   class Calculate
-    {
-
-      public float Result()
-        { 
-      public  string exit = "quit";
-      public  string input;
-
-            do
-            {
-               public  input = Console.ReadLine();
-               public input = input.Replace(" ", "");
-
-                if (input.ToLower() == exit)
-                { Environment.Exit(0); }
-
-   public char[] operators = new char[] { '+', '-', '*', '/' };
-   public  string[] strnumbers = input.Split(operators);
-   public  string[] strOperators = Regex.Split(input, @"\d+");
-
-
-    string FinOpString = "";
-
-                for (int i = 0; i <strOperators.Length; i++)
-                {
-
-                    if (strOperators[i] == "+" || strOperators[i] == "-" || strOperators[i] == "*" || strOperators[i] == "/")
-                    {
-                        FinOpString += strOperators[i];
-                    }
-                }
-
-                public char[] operatorsArray = FinOpString.ToCharArray();
-
-                try
-                {
-
-                    float[] floatNumbers = new float[strnumbers.Length];
-                    for (public int i = 0; i<strnumbers.Length; i++)
-                    {
-                        floatNumbers[i] = float.Parse(strnumbers[i]);
-                    }
-
-
-
-                    public float result = float.Parse(strnumbers[0]);
-public int n = 0;
-                    if (strnumbers.Length == 1)
-                    {
-                       return Console.WriteLine("Please enter more than one value!");
-                    }
-                    else
-                    {
-                        while (n< 1)
-
-                        {
-                            for (public int i = 0; i<operatorsArray.Length; i++)
-                            {
-
-                                if (operatorsArray[i] == '+')
-                                { result += floatNumbers[i + 1]; }
-                                if (operatorsArray[i] == '-')
-                                { result -= floatNumbers[i + 1]; }
-                                if (operatorsArray[i] == '*')
-                                { result *= floatNumbers[i + 1]; }
-                                if (operatorsArray[i] == '/')
-                                { result /= floatNumbers[i + 1]; }
-                                n++;
-                                //Console.WriteLine("Result: {0}\nEnter new values to calculate or quit by typing 'quit'", result);
-                                return result;
-                            }
-
-                        }
-                    }
-                }
-
-                catch (System.OverflowException)
-                {
-                    Console.WriteLine("One or more values is too large for calculations,enter value in the range " +
-                     "of 3.4E +/- 38 and try again!");
-                }
-                catch (System.FormatException)
-                {
-                    Console.WriteLine("Please enter numerical values :) !");
-                }
-               
-            
-         } while (input.ToLower() != exit);
-         
-        }
-    }
-}
    
 
